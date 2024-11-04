809d434690936f4b6aac90c9ca538cf348358e88
jai <jk8142727101@gmail.com>
2024-11-04 18:53:57 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -8,5 +8,22 @@ resource "aws_instance" "db" {
     provisioner "local-exec" {
         command = "echo ${self.private_ip} > private_ips.txt" #self is aws_instance.web
     }
+    # provisioner "local-exec" {
+    #     command = "ansible-playbook -i private_ips.txt web.yaml"
+    # }
 
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo dnf install ansible -y",
+            "sudo dnf install nginx -y",
+            "sudo systemctl start nginx"
+        ]
+    }
 }
